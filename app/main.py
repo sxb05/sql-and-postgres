151b5d0e8a57118c408f292dcefad1cf9c8c0b15
subash <subashbalaji.god@gmail.com>
2026-02-11 15:40:51 +0530

    Implement CRUD operations for products using PostgreSQL

--- app/main.py
+++ app/main.py
@@ -2,81 +2,88 @@ from fastapi import Body, FastAPI,HTTPException,status
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
+import psycopg
+from psycopg.rows import dict_row
+import time
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+
 app = FastAPI()
 
 class  Post(BaseModel):
     name: str
-    description: str
+    inventory: int
     price: int
 
-my_data = [
-    {
-    "name": "laptop",
-    "id": 1,
-    "description": "this is a laptop",
-    "price": 50000
-   } ,{
-        "id": 2,
-    "name": "subash",
-    "description": "person",
-    "price": 1500
-  }
-]
+while True:
+    try:
+        conn = psycopg.connect("dbname=fastapi user=postgres password=12345678" , row_factory=dict_row)
+        cur = conn.cursor()
+        break
+    except Exception as error:
+        print("Error") 
+        time.sleep(20)    
 
+    
 @app.get("/")
-async def root():                     #async keyword is used to perform task asyncronously
-    return "hi++"                     #pydantic has nothing to do with fastapi it is usally used to define a shcemea
+async def root():                        #async keyword is used to perform task asyncronously
+    return "hi++"                        #pydantic has nothing to do with fastapi it is usally used to define a shcemea
 
 
 
-@app.get("/posts")
+@app.get("/products")
 def welcome():
-    return {"data": my_data}
+    x = cur.execute("""SELECT * FROM products""").fetchall()
+    return {"data": x}
 
 
 
 
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_posts(payload: Post):
+@app.post("/products", status_code=status.HTTP_201_CREATED)
+def create_posts(product: Post):
      
-     data_dict = payload.dict()
-     data_dict["id"] = randrange(0, 100000000)
-     my_data.append(data_dict)
-
-     return {"data": data_dict}
-
+    cur.execute("""INSERT INTO products (name , price, inventory) VALUES (%s, %s, %s) RETURNING id""", (product.name, product.price, product.inventory))
+    new_product = cur.fetchone()
+    conn.commit()
+    return {"data": new_product}
 
 
 
 
-@app.get("/posts/{id}")
+
+@app.get("/posts/{id}", status_code=status.HTTP_200_OK)
 def get_post(id: int):
-    print(id)
-    return {id}
-
-
-
-@app.delete("/posts{id}",status_code = status.HTTP_204_NO_CONTENT)
-def del_post(id: int):
-    for i, p in enumerate(my_data):
-        if p["id"] == id:
-            del my_data[i]
-            break
-    else:
+    cur.execute("""SELECT * FROM products WHERE id = %s""",(id,))
+    fet_prod = cur.fetchone()
+    if not fet_prod:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} not found")
-    return my_data
+    return {"data": fet_prod}
+
+
+
+@app.delete("/posts/{id}",status_code=status.HTTP_204_NO_CONTENT)
+def del_post(id: int):
+    cur.execute("""DELETE FROM products WHERE id = %s returning *""", (id,) )
+    deleted_prod = cur.fetchone()
+    if not deleted_prod: 
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"product with id {id} not found")
+    conn.commit()
+    
+    return {"data": f"product with {id} deleted successfully"}
 
 @app.put("/posts/{id}")
-def update_post(id: int, payload: Post):
-    for i, p in enumerate(my_data):
-        if p["id"] == id:
-            posts_dict = payload.dict()
-            posts_dict["id"] = id
-            my_data[i] = posts_dict
-            return my_data[i]
+def update_post(id: int, product: Post):
+    cur.execute("""UPDATE products SET name = %s, price=%s, inventory=%s WHERE id = %s returning id""", (product.name, product.price, product.inventory,id) )
+    updated_prod = cur.fetchone()
+    conn.commit()
+    if not updated_prod:
+         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} not found")
     else:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} not found")
-                
+        return {"data" : "updated sucessfully"}    
 
 
 
